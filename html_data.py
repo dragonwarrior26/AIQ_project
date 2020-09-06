454a65378fd8f03a6e944c35dac6d6d2b376f7bc
Aayush Sharma <44519020+dragonwarrior26@users.noreply.github.com>
2020-09-06 23:42:14 +0530

    Add files via upload

---
+++ html_data.py
@@ -0,0 +1,37 @@
+import os 
+import time
+import requests
+import sys
+
+
+def retrieve_html():
+    
+    for year in range(2011, 2020):
+        for month in range(1, 13):
+            
+            if month < 10:
+                url = 'https://en.tutiempo.net/climate/0{}-{}/ws-421810.html'.format(month, year)
+                
+            else:
+                url = 'https://en.tutiempo.net/climate/{}-{}/ws-421810.html'.format(month, year)
+                
+            texts = requests.get(url)
+        
+            text_utf = texts.text.encode('utf=8')
+        
+        
+            if not os.path.exists('DATA/Html_data/{}'.format(year)):
+                os.makedirs('DATA/Html_data/{}'.format(year))
+        
+            with open('DATA/Html_data/{}/{}.html'.format(year, month), 'wb') as output:
+                output.write(text_utf)
+            
+        sys.stdout.flush()
+        
+        
+if __name__ == "__main__":
+    start_time = time.time()
+    retrieve_html()
+    stop_time = time.time()
+    
+    print("Time Taken {}:".format(start_time - stop_time))
